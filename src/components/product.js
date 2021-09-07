65fa985a28ffa07aae4da48cb6db8c2a15dc3027
koretskiyav <koretskiyav@gmail.com>
2021-09-07 21:32:43 +0300

    add css and svg

--- src/components/product.js
+++ src/components/product.js
@@ -1,13 +1,22 @@
 import counter from '../hocs/counter';
 
+import style from './product.module.css';
+
+import { ReactComponent as Minus } from '../icons/minus.svg';
+import { ReactComponent as Plus } from '../icons/plus.svg';
+
 function Product({ product, amount, decrement, increment }) {
   return (
-    <div>
+    <div className={style.card}>
       <p>{product.name}</p>
       <p>{product.price} $</p>
-      <button onClick={decrement}>-</button>
+      <button onClick={decrement}>
+        <Minus className={style.icon} />
+      </button>
       <span>{amount}</span>
-      <button onClick={increment}>+</button>
+      <button onClick={increment}>
+        <Plus className={style.icon} />
+      </button>
     </div>
   );
 }
